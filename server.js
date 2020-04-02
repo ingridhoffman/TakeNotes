c8d44e58835d2a17f1e6f6fad193a3749b09472c
trapezechic <ingridhoffman@me.com>
2020-04-02 12:28:34 -0700

    give notes unique id

--- server.js
+++ server.js
@@ -17,28 +17,29 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
 app.get("/notes", (req, res) => {
 	res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// API routes
+// Path to database file for API calls
 const dbPath = path.join(__dirname, "./db/db.json");
-
+// API route - get notes
 app.get("/api/notes", (req, res) => {
 	res.sendFile(dbPath);
 });
-
 // API route - save new note
 app.post("/api/notes", (req, res) => {
-	let newNote = req.body;
-	console.log(newNote);
+	// get note from request body
+	const newNote = req.body;
+	// add timestamp as note ID
+	const newID = Date.now();
+	newNote.ID = newID;
+	// get existing database file
 	fs.readFile(dbPath, (err, data) => {
 		if (err) throw err;
 		let notes = JSON.parse(data);
-		console.log(notes);
+		// add new note
 		notes.push(newNote);
-		console.log(notes);
 		let writeNotes = JSON.stringify(notes);
 		fs.writeFile(dbPath, writeNotes, err => {
 			if (err) throw err;
